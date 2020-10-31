f0210eda847484aca0599290e0cef77d41048479
sarahsidek <sarahsidek984@gmail.com>
2020-10-31 16:52:41 +0800

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:fyp/maps/google_maps.dart';
-import 'package:fyp/screen/login.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
--- lib/maps/google_maps.dart
+++ lib/maps/google_maps.dart
@@ -11,7 +11,7 @@ class _GoogleMapsState extends State<GoogleMaps> {
 
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(3.126613, 101.598443);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
