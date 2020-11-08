746faf16f13cd6b2f818a378795d275fe19f5ec4
sarahsidek <sarahsidek984@gmail.com>
2020-11-08 14:16:19 +0800

    latest

--- lib/maps/geolocation.dart
+++ lib/maps/geolocation.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:geocoder/services/base.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoder/geocoder.dart' as geoCo;
@@ -87,35 +86,7 @@ class _GeolocationState extends State<Geolocation> {
                   zoom: 15.0),
               markers: Set<Marker>.of(markers.values)
           ),
-          Positioned(
-            top: 30.0,
-            right: 15.0,
-            left: 15.0,
-            child: Container(
-              height: 50.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter Address",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: (){},
-                    iconSize: 30.0),
-                    ),
-                   onChanged: (val){
-                  setState(() {
-                    searchAddr = val;
-                  });
-                    },
-                ),
-              )
-              ),
+
         ],
       );
   }
